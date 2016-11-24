a8b9390e6fb59a3766aa830310f325d3b83e0989
meganutexas <texasmentalhealth@gmail.com>
2016-11-24 13:54:03 -0600

    abc

--- email.pl
+++ email.pl
@@ -1,25 +1,40 @@
 #!/usr/bin/perl -T
+# use strict;
+# use warnings;
+# use 5.008;
+
+# use Data::Dumper;
+# use CGI;
+# my $q = CGI->new;
+ 
+# $to = 'texasmentalhealth@gmail.com';
+
+# $from = $q->param('email');
+# $subject = $q->param('subject');
+# $message = $q->param('message');;
+ 
+# open(MAIL, "|/usr/sbin/sendmail -t");
+ 
+# # Email Header
+# print MAIL "To: $to\n";
+# print MAIL "From: $from\n";
+# print MAIL "Subject: $subject\n\n";
+# # Email Body
+# print MAIL $message;
+
+# close(MAIL);
+
 use strict;
-use warnings;
-use 5.008;
+use Email::Sender::Simple qw(sendmail);
+use Email::Simple;
+use Email::Simple::Creator;
 
-use Data::Dumper;
-use CGI;
-my $q = CGI->new;
- 
-$to = 'texasmentalhealth@gmail.com';
-
-$from = $q->param('email');
-$subject = $q->param('subject');
-$message = $q->param('message');;
- 
-open(MAIL, "|/usr/sbin/sendmail -t");
- 
-# Email Header
-print MAIL "To: $to\n";
-print MAIL "From: $from\n";
-print MAIL "Subject: $subject\n\n";
-# Email Body
-print MAIL $message;
-
-close(MAIL);
+my $email = Email::Simple->create(
+header => [
+  To      => '"Xavier Q. Ample" <x.ample@example.com>',
+  From    => '"Bob Fishman" <orz@example.mil>',
+  Subject => "don't forget to *enjoy the sauce*",
+],
+body => "This message is short, but at least it's cheap.\n",
+);
+sendmail($email);
