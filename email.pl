0236ea30f88f89c7fda5ffed0dfbead2201f81ae
meganutexas <texasmentalhealth@gmail.com>
2016-11-24 13:55:44 -0600

    abc

--- email.pl
+++ email.pl
@@ -31,9 +31,9 @@ use Email::Simple::Creator;
 
 my $email = Email::Simple->create(
 header => [
-  To      => '"Xavier Q. Ample" <x.ample@example.com>',
-  From    => '"Bob Fishman" <orz@example.mil>',
-  Subject => "don't forget to *enjoy the sauce*",
+  To      => '"Texas Mental Health" <texasmentalhealth@gmail.com>',
+  From    => '"Anon" <anon@example.com>',
+  Subject => "Testing",
 ],
 body => "This message is short, but at least it's cheap.\n",
 );
