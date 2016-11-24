b1d2a5c3f2661844a88c838c4ac790473addfda1
meganutexas <texasmentalhealth@gmail.com>
2016-11-24 14:35:19 -0600

    abc

--- js/load.js
+++ js/load.js
@@ -10,4 +10,8 @@ $(function(){
 	$("#common").load("menus/common.html");
 	$("#docs").load("menus/docs.html");
 	$("#social").load("menus/social.html");
+
+	$("#common_2").load("../menus/common.html");
+	$("#docs_2").load("../menus/docs.html");
+	$("#social_2").load("../menus/social.html");
 });
