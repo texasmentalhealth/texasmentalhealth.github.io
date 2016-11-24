80290441235067930af1413cb712f7c51e528bc2
meganutexas <texasmentalhealth@gmail.com>
2016-11-24 15:05:49 -0600

    abc

--- js/load.js
+++ js/load.js
@@ -10,11 +10,11 @@ $(function(){
 	$("#common").load("menus/common.html");
 	$("#docs").load("menus/docs.html");
 	$("#social").load("menus/social.html");
-	$("#need_to_know").load("menus/need_to_kmow.html");
+	$("#need_to_know").load("menus/need_to_know.html");
 
 	$("#common_2").load("../menus/common.html");
 	$("#docs_2").load("../menus/docs.html");
 	$("#social_2").load("../menus/social.html");
-	$("#need_to_know_2").load("../menus/need_to_kmow.html");
+	$("#need_to_know_2").load("../menus/need_to_know.html");
 
 });
