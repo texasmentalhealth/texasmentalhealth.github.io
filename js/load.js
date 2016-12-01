a146751335cc5a686f04547beb22690223e5676c
Megan Avery <megan1022@utexas.edu>
2016-12-01 12:37:05 -0600

    update

--- js/load.js
+++ js/load.js
@@ -66,4 +66,58 @@ $(function(){
 	$("#ocd-treatments").load("/illnesses/treatments/ocd.html");
 	$("#ptsd-treatments").load("/illnesses/treatments/ptsd.html");
 	$("#schizophrenia-treatments").load("/illnesses/treatments/schizophrenia.html");
+
+	//the therapies pages for the common illnesses
+	$("#add-therapies").load("/illnesses/treatments/therapies/add.html");
+	$("#adhd-therapies").load("/illnesses/treatments/therapies/adhd.html");
+	$("#anorexia-nervosa-therapies").load("/illnesses/treatments/therapies/anorexia_nervosa.html");
+	$("#anxiety-disorders-therapies").load("/illnesses/treatments/therapies/anxiety_disorders.html");
+	$("#autsim-therapies").load("/illnesses/treatments/therapies/autsim.html");
+	$("#avoidand-personality-therapies").load("/illnesses/treatments/therapies/avoidand_personality.html");
+	$("#bipolar-therapies").load("/illnesses/treatments/therapies/bipolar.html");
+	$("#borderline-personality-therapies").load("/illnesses/treatments/therapies/borderline_personality.html");
+	$("#bulimia-therapies").load("/illnesses/treatments/therapies/bulimia.html");
+	$("#dementia-therapies").load("/illnesses/treatments/therapies/dementia.html");
+	$("#depressive-identity-therapies").load("/illnesses/treatments/therapies/depressive_identity.html");
+	$("#dissociative-identity-therapies").load("/illnesses/treatments/therapies/dissociative_identity.html");
+	$("#misophonia-therapies").load("/illnesses/treatments/therapies/misophonia.html");
+	$("#ocd-therapies").load("/illnesses/treatments/therapies/ocd.html");
+	$("#ptsd-therapies").load("/illnesses/treatments/therapies/ptsd.html");
+	$("#schizophrenia-therapies").load("/illnesses/treatments/therapies/schizophrenia.html");
+
+	//the medications pages for the common illnesses
+	$("#add-medications").load("/illnesses/treatments/medications/add.html");
+	$("#adhd-medications").load("/illnesses/treatments/medications/adhd.html");
+	$("#anorexia-nervosa-medications").load("/illnesses/treatments/medications/anorexia_nervosa.html");
+	$("#anxiety-disorders-medications").load("/illnesses/treatments/medications/anxiety_disorders.html");
+	$("#autsim-medications").load("/illnesses/treatments/medications/autsim.html");
+	$("#avoidand-personality-medications").load("/illnesses/treatments/medications/avoidand_personality.html");
+	$("#bipolar-medications").load("/illnesses/treatments/medications/bipolar.html");
+	$("#borderline-personality-medications").load("/illnesses/treatments/medications/borderline_personality.html");
+	$("#bulimia-medications").load("/illnesses/treatments/medications/bulimia.html");
+	$("#dementia-medications").load("/illnesses/treatments/medications/dementia.html");
+	$("#depressive-identity-medications").load("/illnesses/treatments/medications/depressive_identity.html");
+	$("#dissociative-identity-medications").load("/illnesses/treatments/medications/dissociative_identity.html");
+	$("#misophonia-medications").load("/illnesses/treatments/medications/misophonia.html");
+	$("#ocd-medications").load("/illnesses/treatments/medications/ocd.html");
+	$("#ptsd-medications").load("/illnesses/treatments/medications/ptsd.html");
+	$("#schizophrenia-medications").load("/illnesses/treatments/medications/schizophrenia.html");
+
+	//the specialists pages for the common illnesses
+	$("#add-specialists").load("/illnesses/treatments/specialists/add.html");
+	$("#adhd-specialists").load("/illnesses/treatments/specialists/adhd.html");
+	$("#anorexia-nervosa-specialists").load("/illnesses/treatments/specialists/anorexia_nervosa.html");
+	$("#anxiety-disorders-specialists").load("/illnesses/treatments/specialists/anxiety_disorders.html");
+	$("#autsim-specialists").load("/illnesses/treatments/specialists/autsim.html");
+	$("#avoidand-personality-specialists").load("/illnesses/treatments/specialists/avoidand_personality.html");
+	$("#bipolar-specialists").load("/illnesses/treatments/specialists/bipolar.html");
+	$("#borderline-personality-specialists").load("/illnesses/treatments/specialists/borderline_personality.html");
+	$("#bulimia-specialists").load("/illnesses/treatments/specialists/bulimia.html");
+	$("#dementia-specialists").load("/illnesses/treatments/specialists/dementia.html");
+	$("#depressive-identity-specialists").load("/illnesses/treatments/specialists/depressive_identity.html");
+	$("#dissociative-identity-specialists").load("/illnesses/treatments/specialists/dissociative_identity.html");
+	$("#misophonia-specialists").load("/illnesses/treatments/specialists/misophonia.html");
+	$("#ocd-specialists").load("/illnesses/treatments/specialists/ocd.html");
+	$("#ptsd-specialists").load("/illnesses/treatments/specialists/ptsd.html");
+	$("#schizophrenia-specialists").load("/illnesses/treatments/specialists/schizophrenia.html");
 });
