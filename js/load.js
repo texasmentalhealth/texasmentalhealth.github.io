c735df59ae07808e82b6b5f7b7d57b86b41fb953
Megan Avery <megan1022@utexas.edu>
2016-12-01 11:32:33 -0600

    update

--- js/load.js
+++ js/load.js
@@ -7,15 +7,9 @@ $(function () {
 })
 
 $(function(){
-	$("#common").load("menus/common.html");
-	$("#docs").load("menus/docs.html");
-	$("#social").load("menus/social.html");
-	$("#need_to_know").load("menus/need_to_know.html");
-	$("#resources").load("menus/resources.html");
-
-	$("#common_2").load("../menus/common.html");
-	$("#docs_2").load("../menus/docs.html");
-	$("#social_2").load("../menus/social.html");
-	$("#resources_2").load("../menus/resources.html");
-	$("#need_to_know_2").load("../menus/need_to_know.html");
+	$("#common").load("/menus/common.html");
+	$("#docs").load("/menus/docs.html");
+	$("#social").load("/menus/social.html");
+	$("#need_to_know").load("/menus/need_to_know.html");
+	$("#resources").load("/menus/resources.html");
 });
